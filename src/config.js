71cf9b53b1a68d070a21224f0489a5a912ccbbbd
Eriksson Monteiro <erikssonmonteiro@gmail.com>
2022-04-29 15:59:57 +0100

    use testnet configurations

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 export const WALLET_LOG_SIZE_MAX                       = 1000;
-export const GENESIS_TRANSACTION_ID                    = '2VngVznbdiQ5tqfWqn2NMP8DijqCbLX79Gygo9yYRVFU6iN35h';
-export const GENESIS_SHARD_ID                          = 'qGuUgMMVmaCvqrvoWG6zARjkrujGMpzJmpNhBgz1y3RjBG7ZR';
+export const GENESIS_TRANSACTION_ID                    = 'BbYAZLcxbx6adN3KwHZSTGjE6VpeDhiJ3ZPrXs6EMAGqDPfi5';
+export const GENESIS_SHARD_ID                          = 'AyAC3kjLtjM4vktAJ5Xq6mbXKjzEqXoSsmGhhgjnkXUvjtF2M';
 export const CURRENCY_PAIR_SUMMARY_REFRESH_INTERVAL_MS = 10 * 60000;
 export const DISCORD_URL                               = 'https://discord.com/invite/nb5uaKq4yX';
 export const REPORT_ISSUE_ADDRESS                      = 'tA6KT866gnf9UbqwNwvs67SYPXk4Jgm1Y9z43H5YZKdw0b01H6GPn9imdJCGuoxa5syub43JuW4Fi14DA';
