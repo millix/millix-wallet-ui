3cc3ad8f5c599535e576ebb3e963c935d7bc032a
crank <alex.olkhovskii@404publishing.com>
2022-06-03 16:07:43 +0200

    change REPORT_ISSUE_ADDRESS

--- src/config.js
+++ src/config.js
@@ -3,7 +3,7 @@ export const GENESIS_TRANSACTION_ID                    = '2VngVznbdiQ5tqfWqn2NMP
 export const GENESIS_SHARD_ID                          = 'qGuUgMMVmaCvqrvoWG6zARjkrujGMpzJmpNhBgz1y3RjBG7ZR';
 export const CURRENCY_PAIR_SUMMARY_REFRESH_INTERVAL_MS = 10 * 60000;
 export const DISCORD_URL                               = 'https://discord.com/invite/nb5uaKq4yX';
-export const REPORT_ISSUE_ADDRESS                      = 'tcQAg1vsahrrdzA8KM3SmUqbt5KNV2pz1aVsqdPZ1kxw0b013NGMRi19V46EDABTn8vcy5Jg7NjefhYpJ';
+export const REPORT_ISSUE_ADDRESS                      = 'tA6KT866gnf9UbqwNwvs67SYPXk4Jgm1Y9z43H5YZKdw0b01H6GPn9imdJCGuoxa5syub43JuW4Fi14DA';
 const config                                           = {
     WALLET_LOG_SIZE_MAX,
     GENESIS_TRANSACTION_ID,
